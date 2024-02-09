58bd4e61e4217d57372d45a529c338b942408e94
Artem0202 <artyomkov.19@gmail.com>
2024-02-09 23:40:31 +0200

    Update homework 1.1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 'use strict';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('form');
 const photosList = document.querySelector('ul');
@@ -17,7 +19,6 @@ function searchImages(event) {
     });
     return;
   }
-  console.log(inputContent);
 
   const searchParams = new URLSearchParams({
     key: '42277642-5b5e0c3e2383e813180f7c1aa',
@@ -41,24 +42,58 @@ function searchImages(event) {
 }
 
 function fetchThen(photos) {
-  if (photos.totalHits == 0) {
-    iziToast.error({
-      title: 'Error',
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-    });
-    return;
-  }
-  console.log(photos);
+  photosList.innerHTML = '';
 
-  const markup = photos.hits
+  const louder = `<div class="loader"></div>`;
+  photosList.insertAdjacentHTML('beforebegin', louder);
+  const loaderBox = document.querySelector('.loader');
+
+  const markupGalleryPhotos = photos.hits
     .map(photo => {
       return `
-          <li>
-            <img src=${photo.webformatURL} alt="${photo.tags}">
-          </li>
+        <li class="gallery-item">
+          <a href="${photo.largeImageURL}">
+          <img src=${photo.webformatURL} alt="${photo.tags}">
+          <ul class="photo-info">
+            <li>
+                <h3>Likes</h3>
+                <p>${photo.likes}</p>
+            </li>
+            <li>
+                <h3>Views</h3>
+                <p>${photo.views}</p>
+            </li>
+            <li>
+                <h3>Comments</h3>
+                <p>${photo.comments}</p>
+            </li>
+            <li>
+                <h3>Downloads</h3>
+                <p>${photo.downloads}</p>
+            </li>
+          </ul>
+          </a>
+        </li>
       `;
     })
     .join('');
-  photosList.insertAdjacentHTML('beforeend', markup);
+
+  setTimeout(() => {
+    loaderBox.remove();
+    if (photos.totalHits == 0) {
+      iziToast.error({
+        title: 'Error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+
+    photosList.insertAdjacentHTML('beforeend', markupGalleryPhotos);
+    let lightbox = new SimpleLightbox('.gallery-list a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+    lightbox.refresh();
+  }, 1000);
 }
